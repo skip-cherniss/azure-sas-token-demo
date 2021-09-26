6e9157b21227142f7771409bf26556202ebf7d5c
Skip Cherniss <skip-cherniss@users.noreply.github.com>
2021-09-26 05:24:25 -0400

    Update Program.cs

--- src/sas-token-demo/sas-token-demo.console/Program.cs
+++ src/sas-token-demo/sas-token-demo.console/Program.cs
@@ -9,7 +9,15 @@ namespace sas_token_demo.console
         {
             Console.WriteLine("Hello World!");
 
-            string stsuri = "https://stvstudiohsldsasdemo.table.core.windows.net/fighterjets?sv=2020-04-08&ss=t&srt=sco&st=2021-09-26T01%3A41%3A01Z&se=2021-09-27T01%3A41%3A01Z&sp=rwdlacu&sig=53sj6qOnEZHUoHf0GDR5dChhVNq52MWeS9cCpaZbOnc%3D";
+            string stsuri = 
+                "https://stvstudiohsldsasdemo.table.core.windows.net/fighterjets" + 
+                "?sv=2020-04-08" + 
+                "&ss=t" + 
+                "&srt=sco" + 
+                "&st=2021-09-26T01%3A41%3A01Z" +
+                "&se=2021-09-27T01%3A41%3A01Z" +
+                "&sp=rwdlacu&sig=53sj6qOnEZHUoHf0GDR5dChhVNq52MWeS9cCpaZbOnc%3D";
+            
             TableClient tableClient = new TableClient(new Uri(stsuri));
 
             string partitionKey = "usa";
